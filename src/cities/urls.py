f8cfeb1a9c22266b18c2dd8e3e4a6c7c496597ee
garrip91 <45920127+garrip91@users.noreply.github.com>
2021-04-30 23:02:24 +0300

    Summary

--- src/cities/urls.py
+++ src/cities/urls.py
@@ -7,5 +7,5 @@ from cities.views import *
 # Create your tests here:
 urlpatterns = [
     path('', home, name='home'),
-    path('<int:pk>/', home, name='home'),
+    path('detail/<int:pk>/', CityDetailView.as_view(), name='detail'),
 ]
--- src/cities/views.py
+++ src/cities/views.py
@@ -2,19 +2,29 @@ from django.shortcuts import render, get_object_or_404
 
 from cities.models import City
 
+from django.views.generic.detail import DetailView
+
 
 
 __all__ = (
     'home',
+    'CityDetailView',
 )
+
+
 
 # Create your views here:
 def home(request, pk=None):
-    if pk:
-        #city = City.objects.filter(id=pk).first
-        city = get_object_or_404(City, id=pk)
-        context = {'object': city}
-        return render(request, 'cities/detail.html', context)
+    # if pk:
+        # city = get_object_or_404(City, id=pk)
+        # context = {'object': city}
+        # return render(request, 'cities/detail.html', context)
     qs = City.objects.all()
     context = {'objects_list': qs}
     return render(request, 'cities/home.html', context)
+    
+    
+    
+class CityDetailView(DetailView):
+    queryset = City.objects.all()
+    template_name = 'cities/detail.html'
