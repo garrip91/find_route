67373883d4269e8806134ce463cb0a45b93c8017
garrip91 <45920127+garrip91@users.noreply.github.com>
2021-05-05 22:27:13 +0300

    Summary

--- src/cities/views.py
+++ src/cities/views.py
@@ -4,6 +4,8 @@ from cities.models import City
 
 from django.views.generic.detail import DetailView
 
+from cities.forms import HtmlForm, CityForm
+
 
 
 __all__ = (
@@ -15,12 +17,18 @@ __all__ = (
 
 # Create your views here:
 def home(request, pk=None):
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            form.save()
     # if pk:
         # city = get_object_or_404(City, id=pk)
         # context = {'object': city}
         # return render(request, 'cities/detail.html', context)
+    form = CityForm()
     qs = City.objects.all()
-    context = {'objects_list': qs}
+    context = {'objects_list': qs, 'form': form}
     return render(request, 'cities/home.html', context)
     
     
