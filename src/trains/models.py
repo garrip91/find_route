7b2f7c0b0215146dfa32aeb213ef65d43b55850f
garrip91 <45920127+garrip91@users.noreply.github.com>
2021-05-09 22:56:41 +0300

    Update models.py

--- src/trains/models.py
+++ src/trains/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here:
+class Train(models.Model):
+    name = models.CharField(max_length=50, unique=True,
+                            verbose_name='Номер поезда')
+    travel_time = models.PositiveSmallIntegerField(verbose_name='Время в пути')
