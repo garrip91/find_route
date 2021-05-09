a13a7bb5a2137174e3312ce26613f999d880687d
garrip91 <45920127+garrip91@users.noreply.github.com>
2021-05-09 23:02:26 +0300

    Update models.py

--- src/trains/models.py
+++ src/trains/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 
+from cities.models import City
+
+
+
 # Create your models here:
 class Train(models.Model):
     name = models.CharField(max_length=50, unique=True,
                             verbose_name='Номер поезда')
     travel_time = models.PositiveSmallIntegerField(verbose_name='Время в пути')
+    from_city = models.ForeignKey(City, on_delete=)
+    to_city = 
