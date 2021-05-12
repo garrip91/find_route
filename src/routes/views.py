7048eff8cb14e09b01c03e2e775184c92a2d1b81
garrip91 <45920127+garrip91@users.noreply.github.com>
2021-05-12 22:12:40 +0300

    Summary

--- src/routes/views.py
+++ src/routes/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 
-from routes.forms import RouteForm
+from routes.forms import RouteForm, RouteModelForm
 
 from django.contrib import messages
 
 from routes.utils import get_routes
 
 from django.shortcuts import redirect
+
+from trains.models import Train
+from cities.models import City
 
 
 # Create your views here:
@@ -36,6 +39,25 @@ def add_route(request):
     if request.method == 'POST':
         context = {}
         data = request.POST
+        if data:
+            total_time = int(data['total_time'])
+            from_city_id = int(data['from_city'])
+            to_city_id = int(data['to_city'])
+            trains = data['trains'].split(', ')
+            trains_list = [int(t) for t in trains if t.isdigit()]
+            qs = Train.objects.filter(id__in=trains_list).select_related(
+                'from_city', 'to_city')
+            cities = City.objects.filter(
+                id__in=[from_city_id, to_city_id]).in_bulk()
+            form = RouteModelForm(
+                initial={
+                    'from_city': cities[from_city_id],
+                    'to_city': cities[to_city_id],
+                    'travel_times': total_time,
+                    'trains': qs
+                }
+            )
+            context['form'] = form
         return render(request, 'routes/create.html', context)
     else:
         messages.error(request, "Невозможно сохранить несуществующий маршрут!")
